8b57d2c4aef9bfcfcdfb7fdbc10580df399c754c
divyuk <div14kes@gmail.com>
2024-01-18 14:13:34 +0530

    Fixed variable issue🤒

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ import { EnvConfig } from "./types";
 const environmentPath = path.join(__dirname + ".." + ".env");
 dotenv.config({ path: environmentPath });
 export const config: EnvConfig = {
-  HTTPSERVER: parseInt(process.env.HTTP_PORT || "8000", 10),
+  HTTPSERVER: parseInt(process.env.HTTPPORT || "8000", 10),
 };
