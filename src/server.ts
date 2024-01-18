0f1b388c29e42a05089c7928ccf1267044384e2d
divyuk <div14kes@gmail.com>
2024-01-18 14:13:46 +0530

    Setup app

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, { Express } from "express";
 import { config } from "./config";
+import { setupApp } from "./app";
 
 const httpServer: Express = express();
-const httpPort: Number = Number(config.HTTPSERVER);
-httpServer.use(express.json());
 
+const httpPort: Number = Number(config.HTTPSERVER);
+setupApp(httpServer);
 httpServer.listen(httpPort, () => {
   console.log(`HTTP Server started on PORT ${httpPort} ✈✈`);
 });
