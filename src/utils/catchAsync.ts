ba3fae26b378fa046ba318c4b905d750ea7447c5
divyuk <div14kes@gmail.com>
2024-01-19 19:29:27 +0530

    Catch Asyn function

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,10 @@
+import { NextFunction } from "express";
+
+const catchAsyn =
+  (fn: Function) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
