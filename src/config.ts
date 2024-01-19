8f1f9b1c1a3bf1256c06a64df599246f48d998ac
divyuk <div14kes@gmail.com>
2024-01-19 18:07:32 +0530

    Blunder in config file

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { EnvConfig } from "./types";
 
-const environmentPath = path.join(__dirname + ".." + ".env");
+const environmentPath = path.join(__dirname, "..", ".env");
 dotenv.config({ path: environmentPath });
 export const config: EnvConfig = {
   HTTPSERVER: parseInt(process.env.HTTPPORT || "8000", 10),
