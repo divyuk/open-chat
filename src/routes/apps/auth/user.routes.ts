bd3c6a0a4cf4e4200bd12fcf3422a0c269ee4b4a
divyuk <div14kes@gmail.com>
2024-01-29 21:34:00 +0530

    Route for Registration user

--- src/routes/apps/auth/user.routes.ts
+++ src/routes/apps/auth/user.routes.ts
@@ -1,5 +1,7 @@
+import { registerUser } from "@src/controllers/apps/auth/user.controller";
+import { validateRegistration } from "@src/validators/validate";
 import { Router } from "express";
 
 const userRouter = Router();
 
-userRouter.route("/register").post;
+userRouter.route("/register").post(validateRegistration, registerUser);
