1b6c49d0c8ed57675c54d69289d334c1a04f0942
divyuk <div14kes@gmail.com>
2024-01-29 20:51:01 +0530

    The validation system 🐟

---
+++ src/validators/validate.ts
@@ -0,0 +1,22 @@
+import { registerUserSchema } from "@src/schema/userSchema";
+import { validationErrors } from "@src/utils/schemaError";
+import { NextFunction } from "express";
+import { z } from "zod";
+
+function createValidationMiddleware(
+  schema: z.ZodObject<any, any, any> | z.ZodString
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof Error) {
+        const validationError = validationErrors(error);
+        next(new ApiResponse(400, validationError));
+      }
+    }
+  };
+}
+export const validateRegistration =
+  createValidationMiddleware(registerUserSchema);
